61459f14df8d06cba2c75c42bef28ad68400414a
Jacek <ruby.object@googlemail.com>
2013-03-07 00:50:17 +0000

    added html

--- sandstone/sandstone.lisp
+++ sandstone/sandstone.lisp
@@ -1,3 +1,10 @@
 (restas:define-module #:sandstone)
 (in-package #:sandstone)
-(restas:define-route index ("/") "<h1>Hello World</h1>")
+(restas:define-route index ("/")
+  "<html>
+<head><title>Restas</title></head>
+<body>
+<h1>Hello World!</h1>
+<p>Try to use restas.</p>
+</body>
+</html>")
