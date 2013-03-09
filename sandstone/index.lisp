8c2e454e26b6909a9681216e48702edd00d74964
Jacek Podkanski <ruby.object@googlemail.com>
2013-03-09 16:01:26 +0000

    partial views work now

--- sandstone/index.lisp
+++ sandstone/index.lisp
@@ -1,5 +1,14 @@
 (in-package #:sandstone)
 
+
+(defun partial-view (a1)
+  (if (equal a1 "zzz")
+      (who:with-html-output-to-string (out)
+	(:div (:b "got it")))
+      (who:with-html-output-to-string (out)
+	(:div (:i "don't get it"))))
+)
+
 (defun view (arg1 arg2)
   (who:with-html-output-to-string (out)
     (:html
@@ -12,7 +21,7 @@
       (:h1 :id "heading" "Hello Lisp World")
       (:p :class "message"  "Hi everybody, we have success at last. I've made restas run on Heroku.")
       (:p (who:fmt "~s ~s" arg1 arg2)) 		   
-      (:p (if (> 1 2) 
-	      "other choice"
-	      "different choice"))
+      (:p (who:fmt "~a" (if (> 1 2) 
+			    (partial-view "abc")
+			    (partial-view "zzz"))))
       (:footer :style "color: white; text-align: center; background:#444;" "&copy; 2013 Jacek Podkanski")))))
